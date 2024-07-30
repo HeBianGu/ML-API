80f47670ace7def62f6d1e866f995be653f8d468
HeBianGu <908293466@qq.com>
2024-07-30 14:51:14 +0800

    ++

---
+++ Source/Models/HeBianGu.Models.Classifications.Sex/SexClassification.cs
@@ -0,0 +1,23 @@
+﻿using HeBianGu_Models_Classifications_Sex;
+
+namespace HeBianGu.Models.Classifications.Sex
+{
+    public class SexClassification
+    {
+        public static IOrderedEnumerable<KeyValuePair<string, float>> PredictAllLabels(byte[] imageBytes)
+        {
+            //var imageBytes = File.ReadAllBytes(@"C:\Users\LENOVO\Pictures\图像分类\人物\0348134dd5de2a80f148522228b33263.jpg");
+            var sampleData = new SexClassificationMLModel.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+            // Make a single prediction on the sample data and print results.
+            return SexClassificationMLModel.PredictAllLabels(sampleData);
+        }
+
+        public static string PredictLabel(byte[] imageBytes)
+        {
+            return PredictAllLabels(imageBytes)?.FirstOrDefault().Key;
+        }
+    }
+}
---
+++ Source/Tests/HeBianGu.Tests.Main/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using HeBianGu.Models.Classifications.Sex;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace HeBianGu.Tests.Main
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory; //设置初始路径
+            openFileDialog.Filter = "JPG文件(*.jpg)|*.jpg|PNG文件(*.png)|*.csv|所有文件(*.*)|*.*"; //设置“另存为文件类型”或“文件类型”框中出现的选择内容
+            openFileDialog.FilterIndex = 2; //设置默认显示文件类型为Csv文件(*.csv)|*.csv
+            openFileDialog.Title = "打开文件"; //获取或设置文件对话框标题
+            openFileDialog.RestoreDirectory = true; //设置对话框是否记忆上次打开的目录
+            openFileDialog.Multiselect = false;//设置多选
+            if (openFileDialog.ShowDialog() != true) 
+                return;
+            //var imageBytes = System.IO.File.ReadAllBytes(@"C:\Users\LENOVO\Pictures\图像分类\人物\0348134dd5de2a80f148522228b33263.jpg");
+            var imageBytes = System.IO.File.ReadAllBytes(openFileDialog.FileName);
+            var result = SexClassification.PredictLabel(imageBytes);
+            this.btn.Content=result;
+        }
+    }
+}
